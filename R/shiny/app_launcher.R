41eb009e032debdf3a9d8f803bfde64f0e456e85
ChaokunHong <chaokunhong@gmail.com>
2025-04-05 00:28:45 +0800

    Auto-sync changes at 2025-04-05 00:28:45

--- R/shiny/app_launcher.R
+++ R/shiny/app_launcher.R
@@ -1,1 +1,27 @@
- 
+#' Launch the METEOR Shiny Application
+#'
+#' This function launches the METEOR Shiny application for interactive exploration
+#' of antimicrobial resistance data.
+#'
+#' @param ... Additional arguments passed to shiny::runApp
+#'
+#' @return The Shiny application object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Launch the application
+#' launch_meteor()
+#' 
+#' # Launch with specific options
+#' launch_meteor(port = 4000, host = "0.0.0.0")
+#' }
+launch_meteor <- function(...) {
+  app_dir <- system.file("shiny-apps", "meteor-app", package = "meteor")
+  
+  if (app_dir == "") {
+    stop("Could not find the Shiny application directory. Try reinstalling the 'meteor' package.")
+  }
+  
+  shiny::runApp(app_dir, ...)
+} 
